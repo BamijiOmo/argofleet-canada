0cdaff9a8ea17b53e499082d1e1ece19afd86288
bamijiomo <bamijiomo@gmail.com>
2025-11-03 08:18:18 +0000

    Add payment-service source code and ArgoCD config

---
+++ payment-service/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Payment Service is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Payment Service listening on port ${PORT}`);
+});
+
